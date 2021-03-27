4b935ee2f58a94542d6a10b556880d67d6b78de8
mrstarmech <mrstarmech@users.noreply.github.com>
2021-03-27 11:03:17 +0700

    "traces of disposal"->"use-wear traces" (#2)

--- frontend/messages/ru/find.php
+++ frontend/messages/ru/find.php
@@ -1,1 +1,17 @@
-<?phpreturn [    'Description' => 'Описание',    'Manufacturing technique' => 'Техника изготовления',    'Traces of disposal' => 'Следы утилизации',    'Storage location' => 'Место хранения',    'Inventory number' => 'Инвентарный номер',    'The Museum KAMIS' => 'Музейная КАМИС',    'Size' => 'Размеры',    'Material' => 'Материал',    'Dating' => 'Датировка',    'Culture' => 'Культура',    'The author of the excavations' => 'Автор раскопок',    'Links' => 'Ссылки',    'Publications' => 'Публикации',];
+<?php
+
+return [
+    'Description' => 'Описание',
+    'Manufacturing technique' => 'Техника изготовления',
+    'Use-wear traces' => 'Следы утилизации',
+    'Storage location' => 'Место хранения',
+    'Inventory number' => 'Инвентарный номер',
+    'The Museum KAMIS' => 'Музейная КАМИС',
+    'Size' => 'Размеры',
+    'Material' => 'Материал',
+    'Dating' => 'Датировка',
+    'Culture' => 'Культура',
+    'The author of the excavations' => 'Автор раскопок',
+    'Links' => 'Ссылки',
+    'Publications' => 'Публикации',
+];
