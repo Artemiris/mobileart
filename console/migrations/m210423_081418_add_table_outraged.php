5c347bd978bee8326b6bab58629499b16e0b6515
mrstarmech <mr.starmech@gmail.comr>
2021-04-23 15:16:09 +0700

    add migration

---
+++ console/migrations/m210423_081418_add_table_outraged.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210423_081418_add_table_outraged
+ */
+class m210423_081418_add_table_outraged extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210423_081418_add_table_outraged cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
