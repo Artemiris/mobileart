c99d410a2e7a3b6f3f7fc327fcdca9d93b3443fb
mrstarmech <mrstarmech@users.noreply.github.com>
2021-04-23 15:44:53 +0700

    Fix migration safedown (#5)
    Применил без тестирования

--- console/migrations/m210421_011820_add_field_page_author_to_find_language.php
+++ console/migrations/m210421_011820_add_field_page_author_to_find_language.php
@@ -20,8 +20,7 @@ class m210421_011820_add_field_page_author_to_find_language extends Migration
      */
     public function safeDown()
     {
-        $this->addColumn('find_language', 'author_page');
-        return false;
+        $this->dropColumn('find_language', 'author_page');
     }
 
     /*
