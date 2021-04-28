c15ce9f53d8e5ea807ad6e690baa3f1ebeee397a
mrstarmech <mr.starmech@gmail.comr>
2021-04-28 16:29:35 +0700

    fix hardlink

--- frontend/views/report/list.php
+++ frontend/views/report/list.php
@@ -9,7 +9,7 @@ use common\models\ReportRecord;
 <?php if (is_array($r_records) && !empty($r_records)):?>
     <?php foreach ($r_records as $record):?>
         <div class="row btn btn-block" style="border-radius: 0.4em; border: 0.1em solid #3c3c3c;">
-            <a href="http://mobileart/report/view/<?= $record->id ?>">
+            <a href="<?= \yii\helpers\Url::toRoute(['report/view', 'id' => $record->id])?>">
                 <div class="col-xs-1"><?= $record->id ?></div>
                 <div class="col-xs-3"><?= $record->fb_name ?></div>
                 <div class="col-xs-8"><?= $record->page_ref ?></div>
