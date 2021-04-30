9fd15352d3785cdb76b42a85cf1e557d7bc02092
mrstarmech <mr.starmech@gmail.comr>
2021-04-30 13:16:46 +0700

    merge migrations

--- console/migrations/m210430_060749_image_fields_all_in_one.php
+++ console/migrations/m210430_060749_image_fields_all_in_one.php
@@ -3,21 +3,26 @@
 use yii\db\Migration;
 
 /**
- * Class m210428_093709_create_table_find_image_language
+ * Class m210430_060749_image_fields_all_in_one
  */
-class m210428_093709_create_table_find_image_language extends Migration
+class m210430_060749_image_fields_all_in_one extends Migration
 {
     /**
      * {@inheritdoc}
      */
     public function safeUp()
     {
+        $this->addColumn('find_language', 'image_author', $this->string());
+        $this->addColumn('find_language', 'image_copyright', $this->string());
+        $this->addColumn('find_language', 'image_source', $this->string());
+
         $this->createTable('find_image_language',[
             'id' => $this->primaryKey(),
             'find_image_id' => $this->integer()->notNull(),
             'locale' => $this->string(10)->notNull(),
             'image_author' => $this->string(),
             'image_copyright' => $this->string(),
+            'image_source' => $this->string()
         ]);
 
         $this->addForeignKey(
@@ -35,6 +40,10 @@ class m210428_093709_create_table_find_image_language extends Migration
      */
     public function safeDown()
     {
+        $this->dropColumn('find_language', 'image_author');
+        $this->dropColumn('find_language', 'image_copyright');
+        $this->dropColumn('find_language', 'image_source');
+
         $this->dropForeignKey(
             'fk-find_image_language-find_image',
             'find_image_language'
@@ -52,7 +61,7 @@ class m210428_093709_create_table_find_image_language extends Migration
 
     public function down()
     {
-        echo "m210428_093709_create_table_find_image_language cannot be reverted.\n";
+        echo "m210430_060749_image_fields_all_in_one cannot be reverted.\n";
 
         return false;
     }
